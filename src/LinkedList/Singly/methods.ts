633ae44507fef7e53c22b3638e2f0b20bc814de9
기경오 <opterk@gmail.com>
2023-01-27 18:16:10 +0000

    Update methods.ts

--- src/LinkedList/Singly/methods.ts
+++ src/LinkedList/Singly/methods.ts
@@ -1,9 +1,9 @@
 import ListNode from "./ListNode";
 
 export function printNodes<T>(node?: ListNode<T>) {
-  let currNode: null | ListNode<T> = node;
+  let currNode = node;
   while (currNode) {
     console.log(currNode.value);
-    currNode = node.next;
+    currNode = currNode.next;
   }
 }
