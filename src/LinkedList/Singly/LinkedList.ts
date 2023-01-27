a5cb033177639f519848ca2fcd3cfad46451e279
기경오 <opterk@gmail.com>
2023-01-27 19:11:39 +0000

    Update LinkedList.ts and index.ts

--- src/LinkedList/Singly/LinkedList.ts
+++ src/LinkedList/Singly/LinkedList.ts
@@ -17,4 +17,39 @@ export default class LinkedList<T> {
     }
     currNode.next = node;
   }
+
+  findNode(value: T) {
+    let currNode = this.head;
+    while (currNode) {
+      if (currNode.value === value) {
+        return currNode;
+      }
+      currNode = currNode.next;
+    }
+    throw Error("Node not Found.");
+  }
+  addAfter(node: ListNode<T>, value: T) {
+    const newNode = new ListNode(value);
+    newNode.next = node.next;
+    node.next = newNode;
+  }
+  deleteNode(node: ListNode<T>) {
+    let currNode = this.head;
+    if (currNode === node) {
+      this.head = node.next;
+      return;
+    }
+    while (currNode) {
+      if (currNode.next === node) {
+        currNode.next = node.next;
+        return;
+      }
+      currNode = currNode.next;
+    }
+  }
+  deleteAfter(node: ListNode<T>) {
+    if (node.next) {
+      node.next = node.next.next;
+    }
+  }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,32 @@
-import { LinkedList, ListNode, methods } from './LinkedList/Singly';
+import { LinkedList, ListNode, methods } from "./LinkedList/Singly";
 
 const sLinkedList = new LinkedList();
+console.log("ADD At HEAD and ADD BACK");
 sLinkedList.addAtHead(1);
 sLinkedList.addAtHead(2);
 sLinkedList.addBack(3);
 methods.printNodes(sLinkedList.head);
+
+console.log("FIND NODE");
+console.log(sLinkedList.findNode(3));
+// sLinkedList.find(5);  // ERROR
+
+console.log("ADD AFTER: 1 after 4");
+const findedNode = sLinkedList.findNode(1);
+sLinkedList.addAfter(findedNode, 4);
+methods.printNodes(sLinkedList.head);
+
+console.log("DELTE NODE: 1");
+sLinkedList.deleteNode(findedNode);
+methods.printNodes(sLinkedList.head);
+
+console.log("DELETE AFTER: 4");
+
+sLinkedList.deleteAfter(sLinkedList.findNode(4));
+methods.printNodes(sLinkedList.head);
+
+console.log("DELETE NODE: head");
+if (sLinkedList.head) {
+  sLinkedList.deleteNode(sLinkedList.head);
+  methods.printNodes(sLinkedList.head);
+}
