61fadc4ea6618146774cc99ec12808b709a784eb
casentino <opterk@gmail.com>
2023-01-30 03:51:00 +0900

    [ANSWER]00001-RemoveLinkedListElements

--- src/LinkedList/Singly/methods.ts
+++ src/LinkedList/Singly/methods.ts
@@ -7,3 +7,17 @@ export function printNodes<T>(node?: ListNode<T>) {
     currNode = currNode.next;
   }
 }
+export function nodesToArray<T>(node?: ListNode<T>) {
+  let arr: T[] = [];
+  let currNode = node;
+  while (currNode) {
+    arr.push(currNode.value);
+    currNode = currNode.next;
+  }
+  return arr;
+}
+export function isEqualArray(a: any[], b: any[]) {
+  const strA = JSON.stringify(a);
+  const strB = JSON.stringify(b);
+  return strA === strB;
+}
